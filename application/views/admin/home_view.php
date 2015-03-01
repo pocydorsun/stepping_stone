27486133adb5145804f1e857eacf718e2c65f1c6
pocydorsun <kondeekee_wave-z@hotmail.com>
2015-03-01 14:24:08 +0700

    ตกแต่ง Angular search

--- application/views/admin/home_view.php
+++ application/views/admin/home_view.php
@@ -5,46 +5,54 @@
 		$attributes = array('class' => 'form-inline');
 		echo form_open('admin/add_user', $attributes);
 		?>
+		<div class="col-sm-6">
 		<div class="form-group">
-			<label for="txtUsername">ชื่อผู้ใช้</label>
+			<!-- <label for="txtUsername">ชื่อผู้ใช้</label> -->
 			<input type="text" class="form-control" name="txtUsername" id="txtUsername" placeholder="กรอกชื่อผู้ใช้">
 		</div>
 		<button type="submit" class="btn btn-success">
 			เพิ่ม
 		</button>
+		</div>
 		</form>
-
 	</div>
 </div>
 
 <br>
 
-Search: <input ng-model="searchText">
-
-<br>
-
-
-
 <div class="container" >
 	<div class="col-sm-6">
-		<a href="#" class="list-group-item active"><strong>รายชื่อ </strong></a>
 
 		<?php $users_json = json_encode($users);?>
 		<ul class="list-group" <?php echo "ng-init='users = $users_json'"; ?>>
 			
+			<!-- SEARCH -->
+			<li class="list-group-item">
+			    <div class="input-group">
+			      <input type="text" class="form-control" ng-model="searchText" placeholder="ค้นหา...">
+			      <span class="input-group-btn">
+			        <button class="btn btn-default" type="button">
+						<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+			        </button>
+			      </span>
+				</div>
+			</li>
+			
+			<!-- LIST USERNAME -->
 			<li class="list-group-item" ng-repeat="user in filtered = (users | filter:searchText)">
 				{{user.username}}
 				
 				<!-- link remove user for admin (CONFIRM by MODAL) -->
 				<div class="pull-right">
-					<a data-toggle="modal" data-msg="ยืนยันการลบผู้ใช้" data-id=<?php echo site_url("admin/remove_user/"); ?>{{"/"+user.id}} data-toggle="modal" class="open-ConfirmDialog" title="Add this item" style="color: red" data-target=".bs-example-modal-sm">
+					<a href="" data-toggle="modal" data-msg="ยืนยันการลบผู้ใช้" data-id=<?php echo site_url("admin/remove_user/"); ?>{{"/"+user.id}} data-toggle="modal" class="open-ConfirmDialog" title="Add this item" style="color: red" data-target=".bs-example-modal-sm">
 						<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
 					</a>
 				</div>
 
 			</li>
-			<li ng-hide="users.length">ไม่มีชื่อผู้ใช้อยู่ในระบบ</li>
-			<li ng-show="filtered == 0">ไม่มีชื่อผู้ใช้อยู่นี้ในระบบ</li>
+			
+			<!-- NO LIST USERNAME -->
+			<li class="list-group-item" ng-show="filtered == 0">ไม่พบข้อมูล</li>
 			
 		</ul>
 	</div>
