ebd6ffcc938b7805c14524709604c30ce1b51db8
PoCyDorSun <kondeekee_wave-z@hotmail.com>
2015-03-01 10:52:03 +0700

    ปุ่มเพิ่มต้นทาง

--- application/views/admin/home_view.php
+++ application/views/admin/home_view.php
@@ -1,66 +1,74 @@
 <div class="container">
 	<h1>ADMIN HOME</h1>
-	
-	<?php 
+	<div>
+		<?php
 		$attributes = array('class' => 'form-inline');
 		echo form_open('admin/add_user', $attributes);
-	 ?>
+		?>
 		<div class="form-group">
 			<label for="txtUsername">ชื่อผู้ใช้</label>
 			<input type="text" class="form-control" name="txtUsername" id="txtUsername" placeholder="กรอกชื่อผู้ใช้">
 		</div>
-		<button type="submit" class="btn btn-success">เพิ่ม</button>
-	</form>
-	
-</div>
+		<button type="submit" class="btn btn-success">
+			เพิ่ม
+		</button>
+		</form>
 
-<br>
-
-<div class="container" >
-	<div class="col-sm-6">
-		<a href="#" class="list-group-item active"><strong>รายชื่อ </strong></a>
-		<ul class="list-group ">
-			
-			<style>
-				.x_user {color:red;}
-			</style>
-			
-			<?php foreach($users as $user) { ?>
-			<li class="list-group-item">
-				<?php echo $user->username; ?> 
-				<div class="pull-right">
-					<a data-toggle="modal" data-id=<?php echo site_url("admin/remove_user/$user->id"); ?> data-toggle="modal" title="Add this item" class="open-ConfirmDialog x_user" data-target=".bs-example-modal-sm" href="#addBookDialog">
-						<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-					</a>
-				</div>
-				
-			</li>
-			<?php } ?>
-			
-		</ul>
 	</div>
-</div>
 
-<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content">
-      	<div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">คำเตือน</h4>
-      </div>
-      <div class="modal-body">ยืนยันการลบผู้ใช้ </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">ยกเลิก</button>
-        <a class="btn btn-danger" name="userId" id="userId" href="#">ยืนยัน</a>
-      </div>
-    </div>
-  </div>
-</div>
+	<br>
 
-<script>
-	$(document).on("click", ".open-ConfirmDialog", function () {
-		var userId = $(this).data('id');
-	    $(".modal-footer #userId").attr("href", userId );
-	});
-</script>
+	<div class="container" >
+		<div class="col-sm-6">
+			<a href="#" class="list-group-item active"><strong>รายชื่อ </strong></a>
+			<ul class="list-group ">
+
+				<style>
+					.x_user {
+						color: red;
+					}
+				</style>
+
+				<?php foreach($users as $user) {
+				?>
+				<li class="list-group-item">
+					<?php echo $user -> username; ?>
+					<div class="pull-right">
+						<a data-toggle="modal" data-id=<?php echo site_url("admin/remove_user/$user->id"); ?> data-toggle="modal" title="Add this item" class="open-ConfirmDialog x_user" data-target=".bs-example-modal-sm" href="#addBookDialog"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span> </a>
+					</div>
+
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
+
+	<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h4 class="modal-title" id="myModalLabel">คำเตือน</h4>
+					</div>
+					<div class="modal-body">
+						ยืนยันการลบผู้ใช้
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">
+							ยกเลิก
+						</button>
+						<a class="btn btn-danger" name="userId" id="userId" href="#">ยืนยัน</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script>
+			$(document).on("click", ".open-ConfirmDialog", function() {
+				var userId = $(this).data('id');
+				$(".modal-footer #userId").attr("href", userId);
+			});
+		</script>
--- application/views/user/user_view.php
+++ application/views/user/user_view.php
@@ -1,36 +1,41 @@
 <div class="container">
 	<h1>USER PAGE<h2>
-</div>
-
-<?php 
+	<div>
+		<?php
 		$attributes = array('class' => 'form-inline');
 		echo form_open('user/add_source', $attributes);
-	 ?>
-
-<div class="container" >
-	<div class="col-sm-6">
-		<a href="#" class="list-group-item active"><strong>รายชื่อต้นทาง </strong></a>
-		<ul class="list-group ">
-			
-			<style>
-				.x_user {color:red;}
-			</style>
-			
-			<?php foreach($sources as $source) { ?>
-			<li class="list-group-item">
-				<?php echo $source->source_name; ?> 
-				<div class="pull-right">
-					<a data-toggle="modal" data-id="" data-toggle="modal" title="Add this item" class="open-ConfirmDialog x_user" data-target=".bs-example-modal-sm" href="#addBookDialog">
-						<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-					</a>
-				</div>
-				
-			</li>
-			<?php } ?>
-			
-		</ul>
+	?>
+		<div class="form-group">
+			<label for="txtUsername">ชื่อผู้ใช้</label>
+			<input type="text" class="form-control" name="txtUsername" id="txtUsername" placeholder="กรอกชื่อผู้ใช้">
+		</div>
+		<button type="submit" class="btn btn-success">
+			เพิ่ม
+		</button>
+		</form>
 	</div>
-</div>
+	<br>
+	<div class="container" >
+		<div class="col-sm-6">
+			<a href="#" class="list-group-item active"><strong>รายชื่อต้นทาง </strong></a>
+			<ul class="list-group ">
 
-	
-<?php print_r($sources); ?> 
+				<style>
+					.x_user {
+						color: red;
+					}
+				</style>
+
+				<?php foreach($sources as $source) {
+				?>
+				<li class="list-group-item">
+					<?php echo $source -> source_name; ?>
+					<div class="pull-right">
+						<a data-toggle="modal" data-id="" data-toggle="modal" title="Add this item" class="open-ConfirmDialog x_user" data-target=".bs-example-modal-sm" href="#addBookDialog"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span> </a>
+					</div>
+
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</div><?php print_r($sources); ?>
