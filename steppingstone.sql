1fd7b5d5691982fd27b128a9513ee297ca7afe58
pocydorsun <kondeekee_wave-z@hotmail.com>
2015-03-05 16:21:50 +0700

    new export DB

--- steppingstone.sql
+++ steppingstone.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2015 at 10:56 AM
+-- Generation Time: Mar 05, 2015 at 10:20 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,13 +23,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cost`
+--
+
+CREATE TABLE IF NOT EXISTS `cost` (
+  `target_id1` int(11) NOT NULL,
+  `target_id2` int(11) NOT NULL,
+  `cost` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cost`
+--
+
+INSERT INTO `cost` (`target_id1`, `target_id2`, `cost`) VALUES
+(8, 9, 5),
+(8, 10, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `target`
 --
 
 CREATE TABLE IF NOT EXISTS `target` (
 `id` int(11) NOT NULL,
-  `target_name` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `target_name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `target`
+--
+
+INSERT INTO `target` (`id`, `target_name`) VALUES
+(8, 'โรงงาน A'),
+(9, 'โรงงาน B'),
+(10, 'โกดัง A'),
+(11, 'โกดัง B'),
+(12, 'ทดสอบ');
 
 -- --------------------------------------------------------
 
@@ -44,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `status` enum('admin','user','','') CHARACTER SET utf8 NOT NULL DEFAULT 'user',
   `firstname` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `lastname` varchar(255) CHARACTER SET utf8 DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -53,11 +84,17 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `status`, `firstname`, `lastname`) VALUES
 (2, 'admin', 'f6fdffe48c908deb0f4c3bd36c032e72', 'admin', '', ''),
 (24, 'userid', '5cc32e366c87c4cb49e4309b75f57d64', 'user', 'สุธี', 'กลิ่นคง'),
-(23, 'adminadmin', '25d55ad283aa400af464c76d713c07ad', 'user', '', '');
+(26, 'testtest', '25d55ad283aa400af464c76d713c07ad', 'user', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cost`
+--
+ALTER TABLE `cost`
+ ADD KEY `target_id1` (`target_id1`,`target_id2`);
 
 --
 -- Indexes for table `target`
@@ -79,12 +116,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `target`
 --
 ALTER TABLE `target`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
