209e7fe749dfaddd63edb87360b18ca7545229e5
PoCyDorSun <kondeekee_wave-z@hotmail.com>
2015-03-24 11:03:39 +0700

    แก้ไขนะ

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,12 +8,17 @@
 
 		<!-- CSS -->
 		<link rel="stylesheet" href=<?php echo base_url("asset/bootstrap/css/bootstrap.min.css"); ?>>
+		<link rel="stylesheet" href=<?php echo base_url("asset/css/mystyle.css"); ?>>
 
 		<!-- JS -->
-		<script src=<?php echo base_url("asset/jquery/jquery.min.js"); ?>></script>
-		<script src=<?php echo base_url("asset/bootstrap/js/bootstrap.min.js"); ?>></script>
-		<script src=<?php echo base_url("asset/angular/angular.min.js"); ?>></script>
-		<script src=<?php echo base_url("asset/angular/myapp.js"); ?>></script>
+		<script src=
+<?php echo base_url("asset/jquery/jquery.min.js"); ?>></script>
+		<script src=
+<?php echo base_url("asset/bootstrap/js/bootstrap.min.js"); ?>></script>
+		<script src=
+<?php echo base_url("asset/angular/angular.min.js"); ?>></script>
+		<script src=
+<?php echo base_url("asset/angular/myapp.js"); ?>></script>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,6 +26,7 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+
 	</head>
 	<body ng-controller="listTableCtrl">
 		<div class="container">
@@ -28,100 +34,103 @@
 				<div class="container-fluid">
 					<!-- Brand and toggle get grouped for better mobile display -->
 
-				    <div class="navbar-header">
-				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				      </button>
-				      
-				      <?php $login_sess = $this->session->userdata('logged_in'); ?>
-				      
-				      <?php if ($login_sess['status'] == 'admin') :?>
-				      <a class="navbar-brand" href=<?php echo site_url("admin");?>>Stepping Stone</a>
-				      
-				      <?php elseif ($login_sess['status'] == 'user') :?>
-				      <a class="navbar-brand" href=<?php echo site_url("user/plan");?>>Stepping Stone</a>
-				      
-				      <?php else : ?>
-				      <a class="navbar-brand" href=<?php echo base_url();?>>Stepping Stone</a>
-				      <?php endif; ?>
-				      
-				    </div>
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+				
+							<?php $login_sess = $this -> session -> userdata('logged_in'); ?>
+
+							<?php if ($login_sess['status'] == 'admin') :?>
+						
+								<a class="navbar-brand my-brand" href=<?php echo site_url("admin"); ?>>โปรแกรมคำนวนรูปแบบการขนส่ง</a>
+							
+							<?php elseif ($login_sess['status'] == 'user') : ?>
+
+								<a  class="navbar-brand my-brand" href=<?php echo site_url("user/plan"); ?>>โปรแกรมคำนวนรูปแบบการขนส่ง</a>
+
+							<?php else : ?>
+
+								<a  class="navbar-brand my-brand" href=<?php echo base_url(); ?>>โปรแกรมคำนวนรูปแบบการขนส่ง</a>
+
+							<?php endif; ?>
+					
+					</div>
 					<!-- Collect the nav links, forms, and other content for toggling -->
-					
+
 					<!-- LOGIN ADMIN -->
-					
+
 					<?php if ($login_sess['status'] == 'admin') :?>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
 							<li>
-								<a href=<?php echo site_url("admin");?>>จัดการผู้ใช้</a>
+								<a href=<?php echo site_url("admin"); ?>>จัดการผู้ใช้</a>
 							</li>
 							<li>
-								<a href=<?php echo site_url("admin/approve");?>>อนุมัติรายงาน</a>
+								<a href=<?php echo site_url("admin/approve"); ?>>อนุมัติรายงาน</a>
 							</li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
 							<li>
-								<a href=<?php echo site_url("admin/change_password");?>>แก้ไขรหัสผ่าน</a>
+								<a href=<?php echo site_url("admin/change_password"); ?>>แก้ไขรหัสผ่าน</a>
 							</li>
 							<li>
-								<a href=<?php echo site_url("logout");?>>ออกจากระบบ</a>
+								<a href=<?php echo site_url("logout"); ?>>ออกจากระบบ</a>
 							</li>
 						</ul>
 					</div><!-- /.navbar-collapse -->
 					<?php endif; ?>
-					
-					
+
 					<!-- LOGIN USER -->
 					<?php if ($login_sess['status'] == 'user') :?>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
 							<li>
-								<a href="<?php echo site_url("user/plan");?>">จัดการแผนการขนส่ง</a>
+								<a href="<?php echo site_url("user/plan"); ?>">จัดการแผนการขนส่ง</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url("user/target");?>">จัดการเป้าหมาย</a>
+								<a href="<?php echo site_url("user/target"); ?>">จัดการเป้าหมาย</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url("user/cost");?>">กำหนดค่าขนส่ง</a>
+								<a href="<?php echo site_url("user/cost"); ?>">กำหนดค่าขนส่ง</a>
 							</li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
 							<li>
-								<a href=<?php echo site_url("user/profile");?>>แก้ไขข้อมูลส่วนตัว</a>
+								<a href=<?php echo site_url("user/profile"); ?>>แก้ไขข้อมูลส่วนตัว</a>
 							</li>
 							<li>
-								<a href=<?php echo site_url("logout");?>>ออกจากระบบ</a>
+								<a href=<?php echo site_url("logout"); ?>>ออกจากระบบ</a>
 							</li>
 						</ul>
 					</div><!-- /.navbar-collapse -->
 					<?php endif; ?>
-					
+
 				</div><!-- /.container-fluid -->
 			</nav>
 		</div>
-		
+
 		<!-- ALERT ERROR MESSAGE -->
 		<?php if ($this->session->flashdata('error_msg')) : ?>
 		<div class="container">
 			<div class="alert alert-warning" role="alert">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  				<span aria-hidden="true">&times;</span>
-				</button><?php echo $this->session->flashdata('error_msg'); ?>
-			</div>	
+					<span aria-hidden="true">&times;</span>
+				</button><?php echo $this -> session -> flashdata('error_msg'); ?>
+			</div>
 		</div>
 		<?php endif; ?>
-		
+
 		<!-- ALERT SUCCESS MESSAGE -->
 		<?php if ($this->session->flashdata('success_msg')) : ?>
 		<div class="container">
 			<div class="alert alert-success" role="alert">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  				<span aria-hidden="true">&times;</span>
-				</button><?php echo $this->session->flashdata('success_msg'); ?>
-			</div>	
+					<span aria-hidden="true">&times;</span>
+				</button><?php echo $this -> session -> flashdata('success_msg'); ?>
+			</div>
 		</div>
 		<?php endif; ?>
